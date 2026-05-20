5846a8ea9302ac893e582d9cbe2846d5c29f06bd
Seksit Champaphan <bmsseksit@gmail.com>
2026-05-20 12:17:01 +0700

    Update

---
+++ Models/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System.Text.Json;
+
+namespace DataVipWeb.Models
+{
+    [Table("bank_accounts")]
+    public class BankAccount : BaseModel
+    {
+        [PrimaryKey("id")]
+        public Guid Id { get; set; }
+
+        [Column("group_name")]
+        public string GroupName { get; set; } = "";
+
+        [Column("bank_name")]
+        public string BankName { get; set; } = "";
+
+        [Column("account_type")]
+        public string AccountType { get; set; } = "";
+
+        [Column("account_name")]
+        public string AccountName { get; set; } = "";
+
+        [Column("owner_name")]
+        public string OwnerName { get; set; } = "";
+
+        [Column("account_number")]
+        public string AccountNumber { get; set; } = "";
+
+        [Column("received_date")]
+        public DateTime? ReceivedDate { get; set; }
+
+        [Column("released_date")]
+        public DateTime? ReleasedDate { get; set; }
+
+        [Column("details")]
+        public JsonElement Details { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("branch")]
+        public string Branch { get; set; } = "";
+
+        [Column("note")]
+        public string Note { get; set; } = "";
+    }
+}
